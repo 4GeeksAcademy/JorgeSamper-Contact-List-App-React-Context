8c58141e4be2eafdb2a7265f2b7dfca52831f305
JSamper <60151922+jorgesamper@users.noreply.github.com>
2023-06-23 19:54:50 +0000

    components design

---
+++ src/js/component/addContact.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export const addContact = () => (
+    <form className="m-4">
+
+	<h1 className="fw-bold">Add a new contact</h1>
+      <div className="mb-3 text-start fw-bold">
+        <label htmlFor="exampleInputEmail1" className="form-label" >Full Name</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Full Name" />
+      </div>
+
+	  <div className="mb-3 text-start fw-bold">
+        <label htmlFor="exampleInputEmail1" className="form-label" >Email</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email" />
+      </div>
+
+	  <div className="mb-3 text-start fw-bold">
+        <label htmlFor="exampleInputEmail1" className="form-label" >Phone</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Phone" />
+      </div>
+
+	  <div className="mb-3 text-start fw-bold">
+        <label htmlFor="exampleInputEmail1" className="form-label" >Address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Address" />
+      </div>
+      
+      
+      <button type="submit" className="btn btn-primary w-100">Save</button>
+	  <a href="#" class="text-primary text-start">or get back to contacts.</a>
+
+	</form>
+);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,32 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/home.css";
+import "../../styles/img.css";
+import image from "../../img/perfil.png"
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+  <div className="text-center mt-5">
+    <h1>Hello Rigo!</h1>
+
+
+<div class="row border m-4 rounded-3">
+  	<div class="col">
+	  <img src={image} alt="Imagen de perfil" className="img-fluid smaller-image" />
+  	</div>
+  	<div class="col text-start text-secondary">
+  		<li class="list-group-item fw-bold">
+				Mike Anamendolla
+			</li>
+			<li class="list-group-item">
+				<i class="fas fa-map-marker"></i> Ubicacion
+			</li>
+			<li class="list-group-item">
+				<i class="fas fa-phone"></i> 478 834 827
+			</li>
+			<li class="list-group-item">
+				<i class="fas fa-envelope"></i> MikeAnamendolla@gmail.com
+			</li>
+  		</div>
 	</div>
+</div>
 );
